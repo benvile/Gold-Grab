41c85c22e6294f7357f4505c69c1dccf0be1a217
benvile <fallingcrash@live.co.uk>
2017-10-17 21:15:06 +0100

    I added a gold bar
    It's in the top corner ;)

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,28 @@ window.onload = function() {
         //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
         //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-        let game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+        let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 		let logo;
+		let gold;
 		
         function preload () {
 
             game.load.image('logo', 'phaser.png');
+			game.load.image('gold', 'Gold-Ingot.png');
 
         }
 
         function create () {
 
-            logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+            //logo.body.collideWorldBounds = true;
+			
+			logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
             logo.anchor.setTo(0.5, 0.5);
 			logo.scale.setTo(0.5,0.5);
+			
+			gold = game.add.sprite(50, 50, 'gold');
+			gold.anchor.setTo(0.5, 0.5);
+			gold.scale.setTo(0.2,0.2);
 
         }
 		
